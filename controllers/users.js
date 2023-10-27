458edceb8495f2645eb150c4e5226f460219c317
hirostory <hiroakisotooka@gmail.com>
2023-10-27 00:56:15 -0400

    got nehal code

--- controllers/users.js
+++ controllers/users.js
@@ -2,8 +2,6 @@ const express = require("express")
 const router = express.Router()
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
-const multer = require("multer")
-const path = require("path")
 
 const User = require("../classes/user")
 
